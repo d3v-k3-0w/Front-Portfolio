ee13f5891a96e0169875207d6f5df2dfa91461fd
d3v-k3-Ow <garciagomezkerwinoliver@gmail.com>
2024-08-19 17:48:39 -0500

    configure draco para optimizar los 3D

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = ({ scaleFactor, position }) => {
-  const computer = useGLTF('./voxel_web/scene.gltf');
+  const computer = useGLTF('./voxel_web/scene.gltf', './draco/');
 
   return (
     <mesh>
